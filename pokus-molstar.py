300e757093324b0713901ca6ae4d0caed0005d9c
Jan Marecek <marecekje@gmail.com>
2024-12-07 14:07:21 +0100

    Added topologi visualization

--- pokus-molstar.py
+++ pokus-molstar.py
@@ -12,18 +12,16 @@ from streamlit_molstar.auto import st_molstar_auto
 
 import streamlit as st
 
-from streamlit.runtime.scriptrunner import get_script_run_ctx
 
-ctx = get_script_run_ctx()
 
 st.set_page_config(layout="wide")
-
-st.write("from remote url")
-files = ["https://files.rcsb.org/download/3PTB.pdb", "https://files.rcsb.org/download/1LOL.pdb"]
-st_molstar_auto(files, key="6", height="320px")
-
 st.write("from local file")
 files = ['6pxm.pdb']
-
 st_molstar_auto(files, key="7", height="320px")
+
+st.write("topologi")
+files = ['6pxm.gro','em-protein.xtc']
+st_molstar(files[0], files[1], key="8", height="320px")
+
+
 # don't close the streamlit app before interrupting the process in terminal
