07d301f8667058e2abfa1d300540f44a3b1a0018
Jan Marecek <marecekje@gmail.com>
2024-12-07 12:47:02 +0100

    Fixed error mesages and addit trial run from ipynb file

--- pokus-molstar.py
+++ pokus-molstar.py
@@ -12,8 +12,7 @@ from streamlit_molstar.auto import st_molstar_auto
 
 import streamlit as st
 
-from streamlit.runtime.scriptrunner import add_script_run_ctx,get_script_run_ctx
-from subprocess import Popen
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 
 ctx = get_script_run_ctx()
 
@@ -27,8 +26,4 @@ st.write("from local file")
 files = ['6pxm.pdb']
 
 st_molstar_auto(files, key="7", height="320px")
-
-process = Popen(['python','pokus-molstar.py'])
-add_script_run_ctx(process,ctx)
-
 # don't close the streamlit app before interrupting the process in terminal
