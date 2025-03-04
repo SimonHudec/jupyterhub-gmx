8c013810f2a6550a65bf10cf541a780787c774b6
Jan Marecek <marecekje@gmail.com>
2025-03-04 16:07:34 +0100

    Actualization of the project. RMSd needs some polishing. Flare not implemented.

--- pokus-molstar.py
+++ pokus-molstar.py
@@ -20,8 +20,9 @@ files = ['6pxm.pdb']
 st_molstar_auto(files, key="7", height="320px")
 
 st.write("topologi")
-files = ['6pxm.gro','em-protein.xtc']
+files = ['6pxm.gro','em-protein.xtc', 'pbc.xtc']
 st_molstar(files[0], files[1], key="8", height="320px")
+st_molstar(files[0], files[2], key="9", height="320px")
 
 
 # don't close the streamlit app before interrupting the process in terminal
